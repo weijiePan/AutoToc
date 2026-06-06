a4df27bb9c239f02e4f3b0cc87d7abca0f5d0df8
weijie(Alex) pan <97200483+weijiePan@users.noreply.github.com>
2026-06-06 19:16:36 -0400

    backend

---
+++ backend/index.js
@@ -0,0 +1,54 @@
+import express from "express"
+import multer from "multer"
+import path from "path"
+import cors from "cors"
+import {annotateDocument} from "./util/convert.js"
+import fs from "fs"
+const port = 3001;
+const server = express();
+const storageLocation = `./import`; 
+const exportLocation = `./export/document.pdf`
+const rootLocation = path.resolve("./");
+//sanitization
+//ensuring file is pdf
+const storageOption = multer.diskStorage(
+    {
+        destination:function(req, file, cb){
+            cb(null, storageLocation);
+        },
+        filename:function(req,file,cb){
+            cb(null, file.fieldname +".pdf");
+        }
+    }
+);
+//multer storage
+const storage = multer({storage:storageOption});
+//setup server
+server.use(cors({
+    origin:"http://localhost:3000"
+}))
+server.listen(port, ()=>{
+    console.log(`live on http://localhost:${port}`);
+});
+server.get("/", (req,res)=>{
+    res.send("hi");
+} );
+    //post request
+
+server.post("/", storage.single("document"),(req,res)=>{
+   annotateDocument(1,10).then((result)=>{
+    if(result){
+        res.sendFile(exportLocation,{root:rootLocation}, function(err){
+            if(err){
+                console.error("error:", err);
+            }else{
+                console.log("no error, file sent");
+            }
+        })
+    }else{
+        console.log("no file");
+        res.send(false);
+    }
+   })
+});
+
---
+++ backend/util/convert.js
@@ -0,0 +1,32 @@
+import {spawn} from "child_process"
+import path from "path"
+const pythonLocation = "./venv/bin/python" 
+const editorLocation = path.resolve("./util/editor.py");
+const importLocation = path.resolve("./import/document.pdf");
+const exportLocation = path.resolve("./export/document.pdf")
+async function annotateDocument(tocStart, tocEnd){
+        return new Promise((resolve, reject)=>{
+            let editing = spawn(pythonLocation, [editorLocation, tocStart, tocEnd, importLocation,exportLocation])
+            editing.stdout.on("data",(data)=>{
+                console.log("item" + data.toString());
+            })
+            editing.stderr.on("data", (err)=>{
+                console.log(err.toString());
+                resolve(false);
+            })
+            editing.on("close", (code)=>{
+                console.log("end");
+                resolve(true)
+            })
+        })
+        
+    
+  
+}
+//argument to pass
+// tocStart = int(sys.argv[1])
+// tocEnd = int(sys.argv[2])
+// importLocation = sys.argv[3]
+// exportLocation =sys.argv[4]
+
+export {annotateDocument};
