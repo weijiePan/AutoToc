b4255394e46d2664644258e7bd242ded89d7c090
weijie(Alex) pan <97200483+weijiePan@users.noreply.github.com>
2026-09-07 23:40:56 -0400

    test fix for cors

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: 'https://mybooklongbackend.gentlebeach-ec9f59b6.eastus.azurecontainerapps.io/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '<https://app.example>' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
